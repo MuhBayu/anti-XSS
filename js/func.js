d67ff7f77fc6c7938cfe1a54e47c72a3743309f1
MuhBayu <bnugraha00@hotmail.com>
2017-03-04 18:19:10 +0700

    Update func.js

--- js/func.js
+++ js/func.js
@@ -1,3 +1,11 @@
+// ---- Content HTML
+var HTML  = "<h1>Test Heading</h1>"
+	HTML += "<script>alert('Jajaja');</script>";
+// ---- Tags ALLOWED
+var allowed = "<b><a><img>";
+
+document.getElementById('text').innerHTML = strip_tags(HTML, allowed);
+
 function strip_tags(str, allow) {
   // making sure the allow arg is a string containing only tags in lowercase (<a><b><c>)
   allow = (((allow || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
